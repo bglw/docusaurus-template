ca70cf63ee8c0f303fdf4df365bd0beb31a7f3c2
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-06-17 13:50:33 +1200

    Setup repository for CloudCannon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,10 +8,6 @@ module.exports = {
   organizationName: 'your-org', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
-//    algolia: {
-//      apiKey: 'YOUR_API_KEY',
-//      indexName: 'YOUR_INDEX_NAME',
-//    },
     navbar: {
       title: 'My Site',
       logo: {
@@ -32,10 +28,6 @@ module.exports = {
       {
         docs: {
           routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // In order to display a link to edit your documents, please change this value.
-          // Refer to Docusaurus v2 documentation for more info.
-          // editUrl: 'http://easydita.com',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -44,13 +36,5 @@ module.exports = {
     ],
   ],
   plugins: [
-    // To enable the local search functionality, uncomment the following code:
-    // [require.resolve('@cmfcmf/docusaurus-search-local'), {
-    // indexDocs: true,
-    //     docsRouteBasePath: '/',
-    //     indexDocSidebarParentCategories: 3,
-    //     indexPages: false,
-    //     language: "en"
-    // }]
   ],
 };
